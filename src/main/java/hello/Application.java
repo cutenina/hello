50e34f5d8e63e50cdc64b92729f79cb5f057f121
nina <1147976635@qq.com>
2018-07-30 16:09:00 +0800

    提交文件

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:Nina
+ * @version:
+ * @Project: springbootdemo
+ * @Package: hello
+ * @Description:
+ * @Date date: 2018/7/30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
